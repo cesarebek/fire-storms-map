0381cb386b7859946ec8640662cd4ef7864ed4f5
Cesare Bek <cesarebek1@gmail.com>
2021-01-04 11:22:47 +0100

    Created the spinner componentt

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,16 +1,7 @@
 <template>
   <div class="mapPage">
     <!-- Loading Spinner -->
-    <div class="lds-roller spinner" v-if="isLoading">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
+    <my-spinner v-if="isLoading"></my-spinner>
     <!-- Map -->
     <div class="map">
       <l-map :zoom="zoom" :center="center" class="map">
@@ -70,8 +61,8 @@ export default {
         const response = await axios.get(
           'https://d1sat59n3r2spx.cloudfront.net/static/s2impact-global-fire-21-28-agosto.json'
         );
-        const data = await response.data;
         this.isLoading = false;
+        const data = await response.data;
         //Assigning a unique ID
         const dataId = data.map((fire) => ({
           ...fire,
@@ -106,95 +97,5 @@ export default {
   height: 70vh;
   width: auto;
   padding: 0 10rem;
-}
-.lds-roller {
-  display: inline-block;
-  position: relative;
-  width: 80px;
-  height: 80px;
-}
-.lds-roller div {
-  animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-  transform-origin: 40px 40px;
-}
-.lds-roller div:after {
-  content: ' ';
-  display: block;
-  position: absolute;
-  width: 7px;
-  height: 7px;
-  border-radius: 50%;
-  background: #fff;
-  margin: -4px 0 0 -4px;
-}
-.lds-roller div:nth-child(1) {
-  animation-delay: -0.036s;
-}
-.lds-roller div:nth-child(1):after {
-  top: 63px;
-  left: 63px;
-}
-.lds-roller div:nth-child(2) {
-  animation-delay: -0.072s;
-}
-.lds-roller div:nth-child(2):after {
-  top: 68px;
-  left: 56px;
-}
-.lds-roller div:nth-child(3) {
-  animation-delay: -0.108s;
-}
-.lds-roller div:nth-child(3):after {
-  top: 71px;
-  left: 48px;
-}
-.lds-roller div:nth-child(4) {
-  animation-delay: -0.144s;
-}
-.lds-roller div:nth-child(4):after {
-  top: 72px;
-  left: 40px;
-}
-.lds-roller div:nth-child(5) {
-  animation-delay: -0.18s;
-}
-.lds-roller div:nth-child(5):after {
-  top: 71px;
-  left: 32px;
-}
-.lds-roller div:nth-child(6) {
-  animation-delay: -0.216s;
-}
-.lds-roller div:nth-child(6):after {
-  top: 68px;
-  left: 24px;
-}
-.lds-roller div:nth-child(7) {
-  animation-delay: -0.252s;
-}
-.lds-roller div:nth-child(7):after {
-  top: 63px;
-  left: 17px;
-}
-.lds-roller div:nth-child(8) {
-  animation-delay: -0.288s;
-}
-.lds-roller div:nth-child(8):after {
-  top: 56px;
-  left: 12px;
-}
-@keyframes lds-roller {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-.spinner {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  z-index: 10;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
 import { routes } from './routes.js';
+import Spinner from './components/Spinner.vue';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
+
+Vue.component('my-spinner', Spinner);
 
 const router = new VueRouter({
   routes,
